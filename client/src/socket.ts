32f31320625ce0da37db346e327643c1d76f364e
Erkyhanma <yenzel.bt@gmail.com>
2025-05-03 14:26:05 -0400

    socket url changed

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
 const URL =
-  process.env.NODE_ENV === "production" ? undefined : "http://localhost:3000";
+  process.env.NODE_ENV === "production"
+    ? "https://online-multiplayer-tic-tac-toe.onrender.com"
+    : "http://localhost:3000";
 export const socket = io(URL);
